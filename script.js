5b02a34a921ed01aa121b2954cc6848ab7079c20
Mikhalyuk Roman <kre-angel@mail.ru>
2021-04-26 06:02:07 +0300

    tz-4

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// скрипт для бургера
+
 let burger  = document.querySelector('.burger');
 let overlay = document.querySelector('.overlay');
 
@@ -13,3 +15,114 @@ function toggleMenu(){
 }
 
 burger.addEventListener('click' , toggleMenu);
+
+// скрипт для отзывов
+
+const findBlockByAlias = alias => {
+  return $(".reviews__item").filter((ndx, item) => {
+    return $(item).attr("data-linked-with") == alias;
+  });
+};
+
+$(".interactive-avatar__link").click((e) => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr("data-open");
+  const itemToShow = findBlockByAlias(target);
+  const curItem = $this.closest(".interactive-avatar");
+
+  itemToShow.addClass("active").siblings().removeClass("active");
+  curItem.addClass("active").siblings().removeClass("active");
+})
+
+// скрипт для аккщрдеона
+
+const openItim = item => {
+  const container = item.closest(".team__item");
+  const contentBlock = container.find(".team__content");
+  const textBlock = contentBlock.find(".team__content-block");
+  const reqHaight = textBlock.height();
+
+  container.addClass("active");
+  contentBlock.height(reqHaight);
+};
+
+const closeEveryItem = container => {
+  const items = container.find(".team__content");
+  const itemContainer = container.find(".team__item");
+
+  itemContainer.removeClass("active");
+  items.height(0);
+};
+
+
+$(".team__title").click ((e) => {
+  const $thisTeam = $(e.currentTarget);
+  const container = $thisTeam.closest(".team");
+  const elemContainer = $thisTeam.closest(".team__item");
+
+  if (elemContainer.hasClass("active")) {
+    closeEveryItem(container);
+  } else {
+    closeEveryItem(container);
+    openItim($thisTeam);
+  }
+});
+
+// слайдер
+
+const slider = $(".products").bxSlider({
+  pager: false,
+  controls: false,
+});
+
+$(".poducts-slider__arrows--direction--prev").click((e) => {
+  e.preventDefault();
+  slider.goToPrevSlide();
+})
+
+$(".poducts-slider__arrows--direction--next").click((e) => {
+  e.preventDefault();
+  slider.goToNextSlide();
+})
+
+// модальное окно
+
+$(".form").submit((e) => {
+  e.preventDefault();
+
+  const form = $(e.currentTarget);
+  const name = form.find("[name='name']");
+  const phone = form.find("[name='phone']");
+  const comment = form.find("[name='comment']");
+  const to = form.find("[name='to']");
+
+  [name, phone, comment, to].forEach(field => {
+
+    if (field.val() == "") {
+      field.addClass("input-error");
+    }
+  })
+
+  $.ajax({
+    url: "https://webdev-api.loftschool.com/sendmail",
+    method: "post",
+    data: {
+      name: name.val(),
+      phone: phone.val(),
+      comment: comment.val(),
+      to: to.val(),
+    }
+  });
+
+  //$.fancybox.open({
+  //  src: "#modal",
+  //  type: "inline"
+  //})
+})
+
+$(".app-submit-btn").click((e) => {
+  e.preventDefault();
+  $.fancybox.close();
+})
