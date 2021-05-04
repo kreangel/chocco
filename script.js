bcd9e6312b77fe34854506a84831d048760ef559
Mikhalyuk Roman <kre-angel@mail.ru>
2021-05-05 01:55:13 +0300

    dom't work

--- script.js
+++ script.js
@@ -409,14 +409,46 @@ ymaps.ready(init);
 // Player
 
 let player;
+const playerContainer = $('.player');
+
+let eventsInit = () => {
+  $(".player__start").click(e => {
+    e.preventDefault();
+
+    if (playerContainer.hasClass("paused")) {
+      playerContainer.removeClass("paused");
+      player.pauseVideo()
+    } else {
+      playerContainer.addClass("paused");
+      player.playVideo();
+    }
+  })
+}
+
+const onPlayerReady = () => {
+  const durationSec = player.getDuration();
+  
+  $(".player__duration-estimate").text(durationSec);
+}
+
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('yt-player', {
     height: '405',
     width: '660',
     videoId: 'LXb3EKWsInQ',
-    //events: {
+    events: {
     //  'onReady': onPlayerReady,
     //  'onStateChange': onPlayerStateChange
-    //}
+    },
+    playerVars: {
+      controls: 0,
+      disablekb: 0,
+      showinfo: 0,
+      rel: 0,
+      autoplay: 0,
+      modestbranding: 0
+    }
   });
 }
+
+eventsInit();
