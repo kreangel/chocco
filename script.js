3c36d867ab50608b4f8fd3131617a0564a2c11ab
Mikhalyuk Roman <kre-angel@mail.ru>
2021-05-05 19:34:21 +0300

    YouTube

--- script.js
+++ script.js
@@ -416,19 +416,85 @@ let eventsInit = () => {
     e.preventDefault();
 
     if (playerContainer.hasClass("paused")) {
-      playerContainer.removeClass("paused");
-      player.pauseVideo()
+      player.pauseVideo();
     } else {
-      playerContainer.addClass("paused");
       player.playVideo();
     }
+  });
+
+  $(".player__playback").click(e => {
+    const bar = $(e.currentTarget);
+    const clickedPosition = e.originalEvent.layerX;
+    const newButtonPositionPercent = (clickedPosition / bar.width()) * 100;
+    const newPlaybackPositionSec = (player.getDuration() / 100) * newButtonPositionPercent;
+
+    $(".player__playback-button").css({
+      left: `${newButtonPositionPercent}%`
+    });
+
+    player.seekTo(newPlaybackPositionSec);
+  });
+
+  $(".player__splash").click(e => {
+    player.playVideo();
   })
 }
 
+const formatTime = timeSec => {
+  const roundTime = Math.round(timeSec);
+
+  const minutes = addZero(Math.floor(roundTime / 60));
+  const seconds = addZero(roundTime - minutes * 60);
+
+  function addZero(num) {
+    return num < 10 ? `0${num}` : num;
+  }
+
+  return `${minutes} : ${seconds}`;
+}
+
 const onPlayerReady = () => {
+  let interval;
   const durationSec = player.getDuration();
   
-  $(".player__duration-estimate").text(durationSec);
+  $(".player__duration-estimate").text(formatTime(durationSec));
+
+  if (typeof interval != 'undefined') {
+    clearInterval(interval);
+  }
+
+  interval = setInterval(() => {
+    const completedSec = player.getCurrentTime();
+    const completedPercent = (completedSec / durationSec) * 100;
+
+    $(".player__playback-button").css({
+      left: `${completedPercent}%`
+    })
+
+    $(".player__duration-completed").text(formatTime(completedSec));
+  }, 1000);
+}
+
+const onPlayerStateChange = event => {
+  /*
+    -1 (воспроизведение видео не начато)
+    0 (воспроизведение видео завершено)
+    1 (воспроизведение)
+    2 (пауза)
+    3 (буферизация)
+    5 (видео подают реплики).
+  */
+  switch (event.data) {
+    case 1:
+      playerContainer.addClass("active");
+      playerContainer.addClass("paused");
+      break;
+
+    case 2:
+      playerContainer.removeClass("active");
+      playerContainer.removeClass("paused");
+      break;
+  }
 }
 
 function onYouTubeIframeAPIReady() {
@@ -437,8 +503,8 @@ function onYouTubeIframeAPIReady() {
     width: '660',
     videoId: 'LXb3EKWsInQ',
     events: {
-    //  'onReady': onPlayerReady,
-    //  'onStateChange': onPlayerStateChange
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange
     },
     playerVars: {
       controls: 0,
